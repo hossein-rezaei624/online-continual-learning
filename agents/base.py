16f0c7a88490f0e602e377ca9a444888749f1e4e
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-08-25 15:17:34 +0330

    Update base.py

--- agents/base.py
+++ agents/base.py
@@ -173,12 +173,12 @@ class ContinualLearner(torch.nn.Module, metaclass=abc.ABCMeta):
                         # may be faster
                         # feature = feature.squeeze(2).T
                         # _, preds = torch.matmul(means, feature).max(0)
-                        print("predicted labels with old:", np.array(self.old_labels)[pred_label.tolist()])
-                        print("predicted labels just:", pred_label)
-                        print("ground truth:", batch_y)
+                        #print("predicted labels with old:", np.array(self.old_labels)[pred_label.tolist()])
+                        #print("ground truth:", batch_y)
                         correct_cnt = (np.array(self.old_labels)[
                                            pred_label.tolist()] == batch_y.cpu().numpy()).sum().item() / batch_y.size(0)
-                        print("we are in if")
+                        where_ = np.where(np.array(self.old_labels)[pred_label.tolist()] == batch_y.cpu().numpy())
+                        print("we are in if", where_)
                     else:
                         logits = self.model.forward(batch_x)
                         _, pred_label = torch.max(logits, 1)
