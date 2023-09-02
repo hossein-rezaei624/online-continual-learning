4c4fa31d6b65cf16a87d7a4e3dd1804ae89c5e1b
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-09-03 00:47:05 +0330

    Update scr.py

--- agents/scr.py
+++ agents/scr.py
@@ -44,6 +44,7 @@ class SupContrastReplay(ContinualLearner):
                 batch_y = maybe_cuda(batch_y, self.cuda)
 
                 for j in range(self.mem_iters):
+                    print("iiiiii", i)
                     print("in the loop batch_y:", batch_y)
                     mem_x, mem_y = self.buffer.retrieve(x=batch_x, y=batch_y)
                     print("in the loop mem_y:", mem_y)
