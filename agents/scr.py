76306c1fe9b972ab1476d1a22a363eaddf95ac9d
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-09-03 16:07:47 +0330

    Update scr.py

--- agents/scr.py
+++ agents/scr.py
@@ -46,7 +46,7 @@ class SupContrastReplay(ContinualLearner):
                 for j in range(self.mem_iters):
                     print("iiiiii", i)
                     print("in the loop batch_y:", batch_y)
-                    print("buffer.current_index innn", buffer.current_index)
+                    print("buffer.current_index innn", self.buffer.current_index)
                     mem_x, mem_y = self.buffer.retrieve(x=batch_x, y=batch_y)
                     print("in the loop mem_y:", mem_y)
 
