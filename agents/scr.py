7d34402a00b83f6bda59e6ea63a3533b01c8a896
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-09-06 02:47:10 +0330

    Update scr.py

--- agents/scr.py
+++ agents/scr.py
@@ -256,8 +256,8 @@ class SupContrastReplay(ContinualLearner):
         #print("self.buffer.buffer_img", self.buffer.buffer_img.shape, type(self.buffer.buffer_img))
         #print("self.buffer.buffer_label", self.buffer.buffer_label.shape, type(self.buffer.buffer_label), self.buffer.buffer_label)
 
-        self.buffer.buffer_img = all_images.to(device)
-        self.buffer.buffer_label = all_labels.to(device)
+        ##self.buffer.buffer_img = all_images.to(device)
+        ##self.buffer.buffer_label = all_labels.to(device)
 
         #print("self.buffer.buffer_img", self.buffer.buffer_img.shape, type(self.buffer.buffer_img))
         #print("self.buffer.buffer_label", self.buffer.buffer_label.shape, type(self.buffer.buffer_label), self.buffer.buffer_label)
