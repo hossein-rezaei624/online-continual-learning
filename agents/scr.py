8b6b0cac191da9ae58783587d52d80df2520c459
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-09-10 16:10:00 +0330

    Update scr.py

--- agents/scr.py
+++ agents/scr.py
@@ -168,7 +168,7 @@ class SupContrastReplay(ContinualLearner):
 
 
         # Number of top values you're interested in
-        top_n = self.params.mem_size//(task_number+1)
+        top_n = (self.params.mem_size//(task_number+1)) + 1
         
         # Find the indices that would sort the array
         sorted_indices_1 = np.argsort(Confidence_mean.numpy())
