15c1ccc207b9cd83158749da6945f64745ee912b
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-09-15 20:14:57 +0330

    Update scr.py

--- agents/scr.py
+++ agents/scr.py
@@ -203,24 +203,24 @@ class SupContrastReplay(ContinualLearner):
         sorted_indices_2 = np.argsort(Variability.numpy())
         
         # Take the last 'top_n' indices (i.e., the top values)
-        #top_indices_1 = sorted_indices_1[:int(0.95*top_n)]
-        #top_indices_2 = sorted_indices_1[-int(0.025*top_n):]
-        #top_indices_3 = sorted_indices_2[-(top_n - (int(0.95*top_n) + int(0.025*top_n))):]
+        top_indices_1 = sorted_indices_1[:(top_n - (int(0.8*top_n) + int(0.1*top_n)))] #hard to learn
+        top_indices_2 = sorted_indices_1[-int(0.1*top_n):] #easy to learn
+        top_indices_3 = sorted_indices_2[-int(0.8*top_n):] #ambigiuous
         
         #print("top_indicesssss", top_indices.shape, top_indices, type(top_indices))
 
-        #top_indices_12 = np.concatenate((top_indices_2, top_indices_3))
-        #top_indices_123 = np.concatenate((top_indices_12, top_indices_1))
+        top_indices_12 = np.concatenate((top_indices_1, top_indices_2))
+        top_indices_123 = np.concatenate((top_indices_12, top_indices_3))
         
-        #top_indices_sorted = top_indices_123
+        top_indices_sorted = top_indices_123
         
 
     
         
         # Take the last 'top_n' indices (i.e., the top values)
-        top_indices_1 = sorted_indices_1[-top_n:]
+        #top_indices_1 = sorted_indices_1[-top_n:]
         
-        top_indices_sorted = top_indices_1[::-1]
+        #top_indices_sorted = top_indices_1[::-1]
         #top_indices_sorted = top_indices_1
         
         #print("top_indices_sorted", top_indices_sorted, top_indices_sorted.shape)
