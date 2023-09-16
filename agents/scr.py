9986d5a352c33a7e15892d860b3c36f075192eab
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-09-16 13:42:05 +0330

    Update scr.py

--- agents/scr.py
+++ agents/scr.py
@@ -45,29 +45,29 @@ class SupContrastReplay(ContinualLearner):
                                        drop_last=True)
 
 
-##        sets = [
-##            [26, 86, 2, 55, 75, 93, 16, 73, 54, 95],
-##            [53, 92, 78, 13, 7, 30, 22, 24, 33, 8],
-##            [43, 62, 3, 71, 45, 48, 6, 99, 82, 76],
-##            [60, 80, 90, 68, 51, 27, 18, 56, 63, 74],
-##            [1, 61, 42, 41, 4, 15, 17, 40, 38, 5],
-##            [91, 59, 0, 34, 28, 50, 11, 35, 23, 52],
-##            [10, 31, 66, 57, 79, 85, 32, 84, 14, 89],
-##            [19, 29, 49, 97, 98, 69, 20, 94, 72, 77],
-##            [25, 37, 81, 46, 39, 65, 58, 12, 88, 70],
-##            [87, 36, 21, 83, 9, 96, 67, 64, 47, 44]
-##        ]
-
         sets = [
-            [2, 8],
-            [4, 9],
-            [1, 6],
-            [7, 3],
-            [0, 5]
+            [26, 86, 2, 55, 75, 93, 16, 73, 54, 95],
+            [53, 92, 78, 13, 7, 30, 22, 24, 33, 8],
+            [43, 62, 3, 71, 45, 48, 6, 99, 82, 76],
+            [60, 80, 90, 68, 51, 27, 18, 56, 63, 74],
+            [1, 61, 42, 41, 4, 15, 17, 40, 38, 5],
+            [91, 59, 0, 34, 28, 50, 11, 35, 23, 52],
+            [10, 31, 66, 57, 79, 85, 32, 84, 14, 89],
+            [19, 29, 49, 97, 98, 69, 20, 94, 72, 77],
+            [25, 37, 81, 46, 39, 65, 58, 12, 88, 70],
+            [87, 36, 21, 83, 9, 96, 67, 64, 47, 44]
         ]
 
+##        sets = [
+##            [2, 8],
+##            [4, 9],
+##            [1, 6],
+##            [7, 3],
+##            [0, 5]
+##        ]
+
         transform_train = transforms.Compose([transforms.ToTensor(),])
-        trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+        trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform_train)
         subset_indices_train = [idx for idx, (_, target) in enumerate(trainset) if target in sets[task_number]]
         subset_loader_train = torch.utils.data.DataLoader(torch.utils.data.Subset(trainset, subset_indices_train),
                                                           batch_size=10, shuffle=False, num_workers=0, drop_last=True)
@@ -201,7 +201,7 @@ class SupContrastReplay(ContinualLearner):
 
         # Number of top values you're interested in
         #top_n = (self.params.mem_size//(task_number+1)) + 1
-        top_n = (self.params.mem_size//(task_number+1))
+        top_n = (self.params.mem_size//(task_number+1)) + 45
 
 
         # Find the indices that would sort the array
