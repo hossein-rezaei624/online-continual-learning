04922ea322ed1fa2c45281c06daf4f8423b43048
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-09-21 02:42:41 +0330

    Update scr.py

--- agents/scr.py
+++ agents/scr.py
@@ -323,7 +323,7 @@ class SupContrastReplay(ContinualLearner):
         #top_indices_sorted = top_indices_1
 
 
-        top_indices_sorted = sorted_indices_1
+        top_indices_sorted = sorted_indices_2
         #top_indices_sorted = sorted_indices_2[::-1]
 
         
