6841645c61bbf7a50894cc65a8fb6aebd13aa6e5
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-09-21 20:35:36 +0330

    Update scr.py

--- agents/scr.py
+++ agents/scr.py
@@ -115,7 +115,6 @@ class SupContrastReplay(ContinualLearner):
 
         transform_train = transforms.Compose([transforms.ToTensor(),])
         train_cache = load_mini_imagenet_cache('datasets/mini_imagenet/mini-imagenet-cache-train.pkl')
-        print(list(train_cache['class_dict'].items())[:1])
 
         trainset = MiniImageNetDataset(train_cache, transform=transform_train)
         subset_indices_train = [idx for idx, (_, target) in enumerate(trainset) if target in sets[task_number]]
