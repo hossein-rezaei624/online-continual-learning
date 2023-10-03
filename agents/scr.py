e0c050564d44cede8f8481c8e6ffde12be6dae1f
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-10-03 18:15:43 +0330

    Update scr.py

--- agents/scr.py
+++ agents/scr.py
@@ -208,11 +208,11 @@ class SupContrastReplay(ContinualLearner):
         #top_indices_sorted = top_indices_1[::-1] #ambiguous
 
 
-        top_indices_sorted = sorted_indices_1 #hard to learn
+        ##top_indices_sorted = sorted_indices_1 #hard to learn
         
         ##top_indices_sorted = sorted_indices_1[::-1] #easy to learn
 
-        ##top_indices_sorted = sorted_indices_2[::-1] #ambiguous
+        top_indices_sorted = sorted_indices_2[::-1] #ambiguous
 
         
         subset_data = torch.utils.data.Subset(train_dataset, top_indices_sorted)
