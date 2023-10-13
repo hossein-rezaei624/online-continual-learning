c4610658470fb334083c229a1bf2ef512582f65f
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-10-13 19:35:45 +0330

    Update resnet1.py

--- models/resnet1.py
+++ models/resnet1.py
@@ -76,7 +76,7 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, nf * 2, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, nf * 4, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, nf * 8, num_blocks[3], stride=2)
-        self.linear = nn.Linear(nf * 2 * block.expansion, num_classes, bias=bias)
+        self.linear = nn.Linear(nf * 8 * block.expansion, num_classes, bias=bias)
 
 
     def _make_layer(self, block, planes, num_blocks, stride):
@@ -92,9 +92,9 @@ class ResNet(nn.Module):
         out = relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
-        #out = self.layer3(out)
-        #out = self.layer4(out)
-        out = avg_pool2d(out, 16)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         return out
 
