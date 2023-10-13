75b51a2386a871486559ef8317ce3394d35e89be
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-10-13 19:51:13 +0330

    Update scr.py

--- agents/scr.py
+++ agents/scr.py
@@ -11,8 +11,8 @@ import numpy as np
 import random
 
 
-#from models.resnet import ResNet18
-from models.resnet1 import *
+from models.resnet import ResNet34
+##from models.resnet1 import *
 import torch.optim as optim
 import matplotlib.pyplot as plt
 from torch.utils.data import ConcatDataset
@@ -110,7 +110,7 @@ class SupContrastReplay(ContinualLearner):
         
 
         device = "cuda"
-        Model_Carto = Reduced_ResNet18(len(unique_classes))
+        Model_Carto = Reduced_ResNet34(len(unique_classes))
         Model_Carto = Model_Carto.to(device)
         criterion_ = nn.CrossEntropyLoss()
         optimizer_ = optim.SGD(Model_Carto.parameters(), lr=0.1,
