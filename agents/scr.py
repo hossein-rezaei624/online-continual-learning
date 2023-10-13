17c029cac6c1391bb85654bb3bc184dd61c7178e
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-10-13 20:19:31 +0330

    Update scr.py

--- agents/scr.py
+++ agents/scr.py
@@ -168,7 +168,7 @@ class SupContrastReplay(ContinualLearner):
             scheduler_.step()
 
         mean_by_class = {class_id: {epoch: torch.mean(torch.tensor(confidences[epoch])) for epoch in confidences} for class_id, confidences in confidence_by_class.items()}
-        std_of_means_by_class = {class_id: torch.std(torch.tensor([mean_by_class[class_id][epoch] for epoch in range(6)])) for class_id, __ in enumerate(unique_classes)}
+        std_of_means_by_class = {class_id: torch.std(torch.tensor([mean_by_class[class_id][epoch] for epoch in range(8)])) for class_id, __ in enumerate(unique_classes)}
         
         ##print("std_of_means_by_class", std_of_means_by_class)
 
