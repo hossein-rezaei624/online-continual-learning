6dedecfd3e7179ba12a1c04ec69c0707cc59cc7c
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-10-13 21:56:45 +0330

    Update scr.py

--- agents/scr.py
+++ agents/scr.py
@@ -123,12 +123,12 @@ class SupContrastReplay(ContinualLearner):
 
 
         # Initializing the dictionaries        
-        confidence_by_class = {class_id: {epoch: [] for epoch in range(10)} for class_id, __ in enumerate(unique_classes)}
+        confidence_by_class = {class_id: {epoch: [] for epoch in range(7)} for class_id, __ in enumerate(unique_classes)}
 
         
         # Training
-        Carto = torch.zeros((10, len(y_train)))
-        for epoch_ in range(10):
+        Carto = torch.zeros((7, len(y_train)))
+        for epoch_ in range(7):
             print('\nEpoch: %d' % epoch_)
             Model_Carto.train()
             train_loss = 0
@@ -168,7 +168,7 @@ class SupContrastReplay(ContinualLearner):
             scheduler_.step()
 
         mean_by_class = {class_id: {epoch: torch.mean(torch.tensor(confidences[epoch])) for epoch in confidences} for class_id, confidences in confidence_by_class.items()}
-        std_of_means_by_class = {class_id: torch.std(torch.tensor([mean_by_class[class_id][epoch] for epoch in range(10)])) for class_id, __ in enumerate(unique_classes)}
+        std_of_means_by_class = {class_id: torch.std(torch.tensor([mean_by_class[class_id][epoch] for epoch in range(7)])) for class_id, __ in enumerate(unique_classes)}
         
         ##print("std_of_means_by_class", std_of_means_by_class)
 
