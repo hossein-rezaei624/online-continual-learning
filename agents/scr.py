3581a92881c6a42c332831383c726b2f83d76758
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-10-19 20:59:49 +0330

    Update scr.py

--- agents/scr.py
+++ agents/scr.py
@@ -39,6 +39,61 @@ class SupContrastReplay(ContinualLearner):
         )
         self.soft_ = nn.Softmax(dim=1)
     
+
+    def distribute_samples(self, probabilities, M):
+        # Normalize the probabilities
+        total_probability = sum(probabilities.values())
+        normalized_probabilities = {k: v / total_probability for k, v in probabilities.items()}
+    
+        # Calculate the number of samples for each class
+        samples = {k: round(v * M) for k, v in normalized_probabilities.items()}
+        
+        # Check if there's any discrepancy due to rounding and correct it
+        discrepancy = M - sum(samples.values())
+        
+        for key in samples:
+            if discrepancy == 0:
+                break
+            if discrepancy > 0:
+                samples[key] += 1
+                discrepancy -= 1
+            else:
+                samples[key] -= 1
+                discrepancy += 1
+
+        return samples
+
+
+    def distribute_excess(self, lst):
+        # Calculate the total excess value
+        total_excess = sum(val - 500 for val in lst if val > 500)
+    
+        # Number of elements that are not greater than 500
+        recipients = [i for i, val in enumerate(lst) if val < 500]
+    
+        num_recipients = len(recipients)
+    
+        # Calculate the average share and remainder
+        avg_share, remainder = divmod(total_excess, num_recipients)
+    
+        lst = [val if val <= 500 else 500 for val in lst]
+        
+        # Distribute the average share
+        for idx in recipients:
+            lst[idx] += avg_share
+        
+        # Distribute the remainder
+        for idx in recipients[:remainder]:
+            lst[idx] += 1
+        
+        # Cap values greater than 500
+        for i, val in enumerate(lst):
+            if val > 500:
+                return distribute_excess(lst)
+                break
+    
+        return lst
+    
     
     def train_learner(self, x_train, y_train):
         self.before_train(x_train, y_train)
@@ -63,10 +118,16 @@ class SupContrastReplay(ContinualLearner):
         
 
         mapping = {value: index for index, value in enumerate(unique_classes)}
+        reverse_mapping = {index: value for value, index in mapping.items()}
+
+
+        # Initializing the dictionaries        
+        confidence_by_class = {class_id: {epoch: [] for epoch in range(4)} for class_id, __ in enumerate(unique_classes)}
+
         
         # Training
-        Carto = torch.zeros((6, len(y_train)))
-        for epoch_ in range(6):
+        Carto = torch.zeros((4, len(y_train)))
+        for epoch_ in range(4):
             print('\nEpoch: %d' % epoch_)
             Model_Carto.train()
             train_loss = 0
@@ -82,9 +143,13 @@ class SupContrastReplay(ContinualLearner):
                 soft_ = self.soft_(outputs)
                 confidence_batch = []
         
+                # Accumulate confidences and counts
                 for i in range(targets.shape[0]):
-                  confidence_batch.append(soft_[i,targets[i]].item())
-                        
+                    confidence_batch.append(soft_[i,targets[i]].item())
+                    
+                    # Update the dictionary with the confidence score for the current class for the current epoch
+                    confidence_by_class[targets[i].item()][epoch_].append(soft_[i, targets[i]].item())
+
                 loss = criterion_(outputs, targets)
                 loss.backward()
                 optimizer_.step()
@@ -101,16 +166,21 @@ class SupContrastReplay(ContinualLearner):
 
             scheduler_.step()
 
+        mean_by_class = {class_id: {epoch: torch.mean(torch.tensor(confidences[epoch])) for epoch in confidences} for class_id, confidences in confidence_by_class.items()}
+        std_of_means_by_class = {class_id: torch.std(torch.tensor([mean_by_class[class_id][epoch] for epoch in range(4)])) for class_id, __ in enumerate(unique_classes)}
+        
+        ##print("std_of_means_by_class", std_of_means_by_class)
 
         Confidence_mean = Carto.mean(dim=0)
         Variability = Carto.std(dim=0)
         
-        #plt.scatter(Variability, Confidence_mean, s = 2)
+        ##plt.scatter(Variability, Confidence_mean, s = 2)
         
-        #plt.xlabel("Variability") 
-        #plt.ylabel("Confidence") 
+        ##plt.xlabel("Variability") 
+        ##plt.ylabel("Confidence") 
         
-        #plt.savefig('scatter_plot.png')
+        ##plt.savefig('scatter_plot.png')
+
                 
         
         # set up model
@@ -171,12 +241,19 @@ class SupContrastReplay(ContinualLearner):
         #top_indices_1 = sorted_indices_1[-top_n:] #easy to learn
         #top_indices_sorted = top_indices_1[::-1] #easy to learn
         
-        top_indices_1 = sorted_indices_2[-top_n:] #ambigiuous
-        top_indices_sorted = top_indices_1[::-1] #ambiguous
+        #top_indices_1 = sorted_indices_2[-top_n:] #ambigiuous
+        #top_indices_sorted = top_indices_1[::-1] #ambiguous
+
+
+        ##top_indices_sorted = sorted_indices_1 #hard to learn
+        
+        ##top_indices_sorted = sorted_indices_1[::-1] #easy to learn
+
+        top_indices_sorted = sorted_indices_2[::-1] #ambiguous
 
         
         subset_data = torch.utils.data.Subset(train_dataset, top_indices_sorted)
-        trainloader_C = torch.utils.data.DataLoader(subset_data, batch_size=self.batch, shuffle=True, num_workers=0)
+        trainloader_C = torch.utils.data.DataLoader(subset_data, batch_size=self.batch, shuffle=False, num_workers=0)
 
         images_list = []
         labels_list = []
@@ -189,8 +266,59 @@ class SupContrastReplay(ContinualLearner):
         all_labels = torch.cat(labels_list, dim=0)
 
 
-        self.buffer.buffer_label[list_of_indices] = all_labels.to(device)
-        self.buffer.buffer_img[list_of_indices] = all_images.to(device)
+        updated_std_of_means_by_class = {k: v.item() for k, v in std_of_means_by_class.items()}
+        
+        ##print("updated_std_of_means_by_class", updated_std_of_means_by_class)
+
+        dist = self.distribute_samples(updated_std_of_means_by_class, top_n)
+
+        
+        num_per_class = top_n//len(unique_classes)
+        counter_class = [0 for _ in range(len(unique_classes))]
+
+        if len(y_train) == top_n:
+            condition = [num_per_class for _ in range(len(unique_classes))]
+            diff = top_n - num_per_class*len(unique_classes)
+            for o in range(diff):
+                condition[o] += 1
+        else:
+            condition = [value for k, value in dist.items()]
+
+
+        check_bound = len(y_train)/len(unique_classes)
+        ##print("check_bound", check_bound)
+        ##print("condition", condition, sum(condition))
+        for i in range(len(condition)):
+            if condition[i] > check_bound:
+                ##print("iiiiiiiii", i)
+                condition = self.distribute_excess(condition)
+                break
+
+        
+        ##print("condition", condition, sum(condition), "top_n", top_n)
+        images_list_ = []
+        labels_list_ = []
+        
+        for i in range(all_labels.shape[0]):
+            if counter_class[mapping[all_labels[i].item()]] < condition[mapping[all_labels[i].item()]]:
+                counter_class[mapping[all_labels[i].item()]] += 1
+                labels_list_.append(all_labels[i])
+                images_list_.append(all_images[i])
+            if counter_class == condition:
+                ##print("yesssss")
+                break
+
+        
+        all_images_ = torch.stack(images_list_)
+        all_labels_ = torch.stack(labels_list_)
+
+        indices = torch.randperm(all_images_.size(0))
+        shuffled_images = all_images_[indices]
+        shuffled_labels = all_labels_[indices]
+        ##print("shuffled_labels.shape", shuffled_labels.shape)
+        
+        self.buffer.buffer_label[list_of_indices] = shuffled_labels.to(device)
+        self.buffer.buffer_img[list_of_indices] = shuffled_images.to(device)
 
         
         self.after_train()
