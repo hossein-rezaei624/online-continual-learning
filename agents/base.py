c4483d6b4a1235c54daeaf4ec12edf57d7f22b61
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-10-22 11:03:42 +0330

    Update base.py

--- agents/base.py
+++ agents/base.py
@@ -165,7 +165,7 @@ class ContinualLearner(torch.nn.Module, metaclass=abc.ABCMeta):
                     print("batch_x.shape" ,batch_x.shape, batch_x[0][0])
                     print(batch_x.cpu().numpy()[0][0])
                     ##batch_x_ = torchvision.transforms.functional.rotate(batch_x, 90)
-                    batch_x_ = torch.tensor(shot_noise(batch_x.permute(0,3,1,2).cpu().numpy())).to("cuda")
+                    batch_x_ = torch.tensor(shot_noise(batch_x.permute(0,2,3,1).cpu().numpy())).to("cuda")
                     
                     # Extract the first 10 images
                     images_1 = [batch_x_[i] for i in range(10)]
