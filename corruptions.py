eee30564d53c173bcc857b3c86341cda6dac2eac
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-01 22:13:06 +0330

    Update corruptions.py

--- corruptions.py
+++ corruptions.py
@@ -147,17 +147,6 @@ def speckle_noise(x, severity=1):
     return np.clip(x + x * np.random.normal(size=x.shape, scale=c), 0, 1) * 255
 
 
-def fgsm(x, source_net, severity=1):
-    c = [8, 16, 32, 64, 128][severity - 1]
-
-    x = V(x, requires_grad=True)
-    logits = source_net(x)
-    source_net.zero_grad()
-    loss = F.cross_entropy(logits, V(logits.data.max(1)[1].squeeze_()), size_average=False)
-    loss.backward()
-
-    return standardize(torch.clamp(unstandardize(x.data) + c / 255. * unstandardize(torch.sign(x.grad.data)), 0, 1))
-
 
 def gaussian_blur(x, severity=1):
     c = [1, 2, 3, 4, 6][severity - 1]
@@ -167,27 +156,21 @@ def gaussian_blur(x, severity=1):
 
 
 def glass_blur(x, severity=1):
-
-    # infer the shape of the image
-    __, h_img, w_img, _ = x.shape
-    
     # sigma, max_delta, iterations
     c = [(0.7, 1, 2), (0.9, 2, 1), (1, 2, 3), (1.1, 3, 2), (1.5, 4, 2)][severity - 1]
-    
-    x = np.uint8(gaussian(np.array(x) / 255., sigma=c[0], channel_axis=-1) * 255)
-    
+
+    x = np.uint8(gaussian(np.array(x) / 255., sigma=c[0], multichannel=True) * 255)
+
     # locally shuffle pixels
     for i in range(c[2]):
-        for h in range(h_img - c[1], c[1], -1):
-            for w in range(w_img - c[1], c[1], -1):
+        for h in range(224 - c[1], c[1], -1):
+            for w in range(224 - c[1], c[1], -1):
                 dx, dy = np.random.randint(-c[1], c[1], size=(2,))
                 h_prime, w_prime = h + dy, w + dx
                 # swap
-                x[:, h, w], x[:, h_prime, w_prime] = x[:, h_prime, w_prime], x[:, h, w]
+                x[h, w], x[h_prime, w_prime] = x[h_prime, w_prime], x[h, w]
 
-    return np.clip(gaussian(x / 255., sigma=c[0], channel_axis=-1), 0, 1) * 255
-    
-
+    return np.clip(gaussian(x / 255., sigma=c[0], multichannel=True), 0, 1) * 255
 
 
 def defocus_blur(x, severity=1):
@@ -198,16 +181,13 @@ def defocus_blur(x, severity=1):
 
     channels = []
     for d in range(3):
-        channels.append(cv2.filter2D(x[:, :, :, d], -1, kernel))
-    #channels = np.array(channels).transpose((1, 2, 0))  # 3x224x224 -> 224x224x3
+        channels.append(cv2.filter2D(x[:, :, d], -1, kernel))
+    channels = np.array(channels).transpose((1, 2, 0))  # 3x224x224 -> 224x224x3
 
     return np.clip(channels, 0, 1) * 255
 
 
 def motion_blur(x, severity=1):
-
-    __, h_img, w_img, _ = x.shape
-    
     c = [(10, 3), (15, 5), (15, 8), (15, 12), (20, 15)][severity - 1]
 
     output = BytesIO()
@@ -241,14 +221,6 @@ def zoom_blur(x, severity=1):
     return np.clip(x, 0, 1) * 255
 
 
-def fog(x, severity=1):
-    c = [(1.5, 2), (2., 2), (2.5, 1.7), (2.5, 1.5), (3., 1.4)][severity - 1]
-
-    x = np.array(x) / 255.
-    max_val = x.max()
-    x += c[0] * plasma_fractal(wibbledecay=c[1])[:224, :224][..., np.newaxis]
-    return np.clip(x * max_val / (max_val + c[0]), 0, 1) * 255
-
 
 def frost(x, severity=1):
     c = [(1, 0.4),
@@ -271,114 +243,6 @@ def frost(x, severity=1):
     return np.clip(c[0] * np.array(x) + c[1] * frost, 0, 255)
 
 
-def snow(x, severity=1):
-    c = [(0.1, 0.3, 3, 0.5, 10, 4, 0.8),
-         (0.2, 0.3, 2, 0.5, 12, 4, 0.7),
-         (0.55, 0.3, 4, 0.9, 12, 8, 0.7),
-         (0.55, 0.3, 4.5, 0.85, 12, 8, 0.65),
-         (0.55, 0.3, 2.5, 0.85, 12, 12, 0.55)][severity - 1]
-
-    x = np.array(x, dtype=np.float32) / 255.
-    snow_layer = np.random.normal(size=x.shape[:2], loc=c[0], scale=c[1])  # [:2] for monochrome
-
-    snow_layer = clipped_zoom(snow_layer[..., np.newaxis], c[2])
-    snow_layer[snow_layer < c[3]] = 0
-
-    snow_layer = PILImage.fromarray((np.clip(snow_layer.squeeze(), 0, 1) * 255).astype(np.uint8), mode='L')
-    output = BytesIO()
-    snow_layer.save(output, format='PNG')
-    snow_layer = MotionImage(blob=output.getvalue())
-
-    snow_layer.motion_blur(radius=c[4], sigma=c[5], angle=np.random.uniform(-135, -45))
-
-    snow_layer = cv2.imdecode(np.fromstring(snow_layer.make_blob(), np.uint8),
-                              cv2.IMREAD_UNCHANGED) / 255.
-    snow_layer = snow_layer[..., np.newaxis]
-
-    x = c[6] * x + (1 - c[6]) * np.maximum(x, cv2.cvtColor(x, cv2.COLOR_RGB2GRAY).reshape(224, 224, 1) * 1.5 + 0.5)
-    return np.clip(x + snow_layer + np.rot90(snow_layer, k=2), 0, 1) * 255
-
-
-def spatter(x, severity=1):
-    c = [(0.65, 0.3, 4, 0.69, 0.6, 0),
-         (0.65, 0.3, 3, 0.68, 0.6, 0),
-         (0.65, 0.3, 2, 0.68, 0.5, 0),
-         (0.65, 0.3, 1, 0.65, 1.5, 1),
-         (0.67, 0.4, 1, 0.65, 1.5, 1)][severity - 1]
-    x = np.array(x, dtype=np.float32) / 255.
-
-    liquid_layer = np.random.normal(size=x.shape[:2], loc=c[0], scale=c[1])
-
-    liquid_layer = gaussian(liquid_layer, sigma=c[2])
-    liquid_layer[liquid_layer < c[3]] = 0
-    if c[5] == 0:
-        liquid_layer = (liquid_layer * 255).astype(np.uint8)
-        dist = 255 - cv2.Canny(liquid_layer, 50, 150)
-        dist = cv2.distanceTransform(dist, cv2.DIST_L2, 5)
-        _, dist = cv2.threshold(dist, 20, 20, cv2.THRESH_TRUNC)
-        dist = cv2.blur(dist, (3, 3)).astype(np.uint8)
-        dist = cv2.equalizeHist(dist)
-        ker = np.array([[-2, -1, 0], [-1, 1, 1], [0, 1, 2]])
-        dist = cv2.filter2D(dist, cv2.CV_8U, ker)
-        dist = cv2.blur(dist, (3, 3)).astype(np.float32)
-
-        m = cv2.cvtColor(liquid_layer * dist, cv2.COLOR_GRAY2BGRA)
-        m /= np.max(m, axis=(0, 1))
-        m *= c[4]
-
-        # water is pale turqouise
-        color = np.concatenate((175 / 255. * np.ones_like(m[..., :1]),
-                                238 / 255. * np.ones_like(m[..., :1]),
-                                238 / 255. * np.ones_like(m[..., :1])), axis=2)
-
-        color = cv2.cvtColor(color, cv2.COLOR_BGR2BGRA)
-        x = cv2.cvtColor(x, cv2.COLOR_BGR2BGRA)
-
-        return cv2.cvtColor(np.clip(x + m * color, 0, 1), cv2.COLOR_BGRA2BGR) * 255
-    else:
-        m = np.where(liquid_layer > c[3], 1, 0)
-        m = gaussian(m.astype(np.float32), sigma=c[4])
-        m[m < 0.8] = 0
-
-        # mud brown
-        color = np.concatenate((63 / 255. * np.ones_like(x[..., :1]),
-                                42 / 255. * np.ones_like(x[..., :1]),
-                                20 / 255. * np.ones_like(x[..., :1])), axis=2)
-
-        color *= m[..., np.newaxis]
-        x *= (1 - m[..., np.newaxis])
-
-        return np.clip(x + color, 0, 1) * 255
-
-
-def contrast(x, severity=1):
-    c = [0.4, .3, .2, .1, .05][severity - 1]
-
-    x = np.array(x) / 255.
-    means = np.mean(x, axis=(0, 1), keepdims=True)
-    return np.clip((x - means) * c + means, 0, 1) * 255
-
-
-def brightness(x, severity=1):
-    c = [.1, .2, .3, .4, .5][severity - 1]
-
-    x = np.array(x) / 255.
-    x = sk.color.rgb2hsv(x)
-    x[:, :, 2] = np.clip(x[:, :, 2] + c, 0, 1)
-    x = sk.color.hsv2rgb(x)
-
-    return np.clip(x, 0, 1) * 255
-
-
-def saturate(x, severity=1):
-    c = [(0.3, 0), (0.1, 0), (2, 0), (5, 0.1), (20, 0.2)][severity - 1]
-
-    x = np.array(x) / 255.
-    x = sk.color.rgb2hsv(x)
-    x[:, :, 1] = np.clip(x[:, :, 1] * c[0] + c[1], 0, 1)
-    x = sk.color.hsv2rgb(x)
-
-    return np.clip(x, 0, 1) * 255
 
 
 def jpeg_compression(x, severity=1):
