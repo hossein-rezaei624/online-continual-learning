b40c8f5779c612e26654b608351f069134b61d97
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-02 01:07:56 +0330

    Update corruptions.py

--- corruptions.py
+++ corruptions.py
@@ -121,7 +121,8 @@ def clipped_zoom(img, zoom_factor):
 
 def gaussian_noise(x, severity=1):
     c = [.08, .12, 0.18, 0.26, 0.38][severity - 1]
-
+    aa = np.array(x)
+    print("aa.shape", aa.shape)
     x = np.array(x) / 255.
     return np.clip(x + np.random.normal(size=x.shape, scale=c), 0, 1) * 255
 
