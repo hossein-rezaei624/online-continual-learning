b324f2de7382c450ee314b24429e0deeed119393
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-02 01:13:10 +0330

    Update base.py

--- agents/base.py
+++ agents/base.py
@@ -170,7 +170,7 @@ class ContinualLearner(torch.nn.Module, metaclass=abc.ABCMeta):
                     batch_x_ = batch_x[0]  # Taking the first image from the batch
                     batch_x_pil = to_pil(batch_x_.cpu())  # Convert to PIL image
                     
-                    
+                    print("gaussian_noise(batch_x_pil).shape", gaussian_noise(batch_x_pil).shape)
                     batch_x1 = torch.tensor(gaussian_noise(batch_x_pil).astype(float) / 255.0, dtype = batch_x.dtype).to("cuda")
                     #batch_x2 = torch.tensor(shot_noise(batch_x_).astype(float) / 255.0, dtype = batch_x.dtype).to("cuda").permute(2,0,1)
                     #batch_x3 = torch.tensor(impulse_noise(batch_x_).astype(float) / 255.0, dtype = batch_x.dtype).to("cuda").permute(2,0,1)
