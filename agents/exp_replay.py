a0ce856535897f6a4131c23079b7cf3edb26e403
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-02 02:33:09 +0330

    Update exp_replay.py

--- agents/exp_replay.py
+++ agents/exp_replay.py
@@ -101,7 +101,7 @@ class ExperienceReplay(ContinualLearner):
         for _, labels, indices_1 in train_loader:
             unique_classes.update(labels.numpy())
         
-        Epoch_Carto = 11
+        Epoch_Carto = 12
         device = "cuda"
         Model_Carto = ResNet18(len(unique_classes))
         Model_Carto = Model_Carto.to(device)
