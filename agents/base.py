1694d6d5e1ac7ec41be51234efdb8274e7db754a
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-02 18:09:50 +0330

    Update base.py

--- agents/base.py
+++ agents/base.py
@@ -169,7 +169,7 @@ class ContinualLearner(torch.nn.Module, metaclass=abc.ABCMeta):
                     to_pil = ToPILImage()
                     batch_x_ = batch_x[0]  # Taking the first image from the batch
                     batch_x_pil = to_pil(batch_x_.cpu())  # Convert to PIL image
-                    print(batch_x_pil.size)
+                    
                     to_tensor_ = PILToTensor()
                     
                     batch_x1 = torch.tensor(gaussian_noise(batch_x_pil).astype(float) / 255.0, dtype = batch_x.dtype).to("cuda").permute(2,0,1).reshape(batch_x.shape)
@@ -183,7 +183,7 @@ class ContinualLearner(torch.nn.Module, metaclass=abc.ABCMeta):
                     ##print("before", to_tensor_(pixelate(batch_x_pil)), to_tensor_(pixelate(batch_x_pil)).to(dtype=batch_x.dtype))
                     ##print("few after", type(batch_x), type(to_tensor_(pixelate(batch_x_pil))), type(to_tensor_(pixelate(batch_x_pil)).to(dtype=batch_x.dtype)))
                     batch_x9 = to_tensor_(pixelate(batch_x_pil)).to(dtype=batch_x.dtype).to("cuda").reshape(batch_x.shape)
-                    ##print("after", batch_x9[0], type(batch_x9))
+                    print("after", to_tensor_(pixelate(batch_x_pil)), dtype(to_tensor_(pixelate(batch_x_pil))), dtype(batch_x), dtype(batch_x9))
                     #batch_x10 = torch.tensor(jpeg_compression(batch_x_pil).astype(float) / 255.0, dtype = batch_x.dtype).to("cuda").permute(2,0,1).reshape(batch_x.shape)
                     
 
