0789b9977f74f5ac1b76f906d9227a507bbabe75
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-03 02:52:51 +0330

    Update base.py

--- agents/base.py
+++ agents/base.py
@@ -12,7 +12,6 @@ import pickle
 
 import torchvision
 from corruptions import *
-from torchvision.transforms import ToPILImage, PILToTensor
 
 class ContinualLearner(torch.nn.Module, metaclass=abc.ABCMeta):
     '''
@@ -138,11 +137,10 @@ class ContinualLearner(torch.nn.Module, metaclass=abc.ABCMeta):
 
                 acc.update(correct_cnt, batch_y.size(0))
                 # Convert tensor to PIL image
-                to_pil = ToPILImage()
-                batch_x_ = batch_x[0]  # Taking the first image from the batch
-                batch_x_pil = to_pil(batch_x_.cpu())  # Convert to PIL image
                 
-                to_tensor_ = PILToTensor()
+                batch_x_ = batch_x[0]  # Taking the first image from the batch
+                batch_x_pil = torchvision.transforms.functional.to_pil_image(batch_x_.cpu())  # Convert to PIL image
+                
                 batch_x1 = torch.tensor(gaussian_noise(batch_x_pil).astype(float) / 255.0, dtype = batch_x.dtype).to("cuda").permute(2,0,1).reshape(batch_x.shape)
             acc_array[task] = acc.avg()
         print(acc_array)
