d713db5cb4e1e53a6127dacb3edb6ee746de58ef
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-03 06:31:23 +0330

    Update corruptions.py

--- corruptions.py
+++ corruptions.py
@@ -1,8 +1,20 @@
 import numpy as np
+import random
+import torch
 
 def gaussian_noise_deterministic(x, severity=1, seed=None):
     if seed is not None:
         np.random.seed(seed)
+
+        np.random.seed(seed)
+        random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+    
     c = [.08, .12, 0.18, 0.26, 0.38][severity - 1]
     x = np.array(x) / 255.
     return np.clip(x + np.random.normal(size=x.shape, scale=c), 0, 1) * 255
