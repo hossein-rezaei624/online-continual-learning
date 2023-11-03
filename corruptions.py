67cd7a1e5d92749590d9cf2ae17aee396144437b
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-03 18:13:15 +0330

    Update corruptions.py

--- corruptions.py
+++ corruptions.py
@@ -2,8 +2,8 @@ import numpy as np
 
 
 def gaussian_noise_deterministic(x, severity=1, seed=None):
-    if seed is not None:
-        np.random.seed(seed)
+    #if seed is not None:
+     #   np.random.seed(seed)
 
     c = [.08, .12, 0.18, 0.26, 0.38][severity - 1]
     x = np.array(x) / 255.
