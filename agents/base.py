e192b99550b52f4a2203aa5e2cf2d06cce0f6f7f
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-03 22:04:58 +0330

    Update base.py

--- agents/base.py
+++ agents/base.py
@@ -176,8 +176,8 @@ class ContinualLearner(torch.nn.Module, metaclass=abc.ABCMeta):
 ##                    batch_x111 = torch.tensor(self.gaussian_noise(batch_x_pil).astype(float) / 255.0, dtype = batch_x.dtype).to("cuda").permute(2,0,1).reshape(batch_x.shape)
 ##
                     
-                    np.random.seed(0)
-                    #torch.manual_seed(0)
+                    #np.random.seed(0)
+                    torch.manual_seed(0)
 
                     
                     
