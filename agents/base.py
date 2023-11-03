44f2bfe2b0f73231e4c6f83eb92b9257a72aa690
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-03 06:10:37 +0330

    Update base.py

--- agents/base.py
+++ agents/base.py
@@ -133,17 +133,29 @@ class ContinualLearner(torch.nn.Module, metaclass=abc.ABCMeta):
                 batch_y = maybe_cuda(batch_y, self.cuda)
                 
                 
-                np.random.seed(0)
-                random.seed(0)
+                #np.random.seed(0)
+                #random.seed(0)
                 
                 
                 #batch_x_ = batch_x[0]  # Taking the first image from the batch
-                batch_x_pil = torchvision.transforms.functional.to_pil_image(torch.randn((3,32,32)).cpu())  # Convert to PIL image
-                torch.tensor(gaussian_noise(batch_x_pil).astype(float) / 255.0, dtype = batch_x.dtype).to("cuda").permute(2,0,1).reshape(batch_x.shape)
+                #batch_x_pil = torchvision.transforms.functional.to_pil_image(torch.randn((3,32,32)).cpu())  # Convert to PIL image
+                #torch.tensor(gaussian_noise(batch_x_pil).astype(float) / 255.0, dtype = batch_x.dtype).to("cuda").permute(2,0,1).reshape(batch_x.shape)
                 
                 
-                np.random.seed(0)
-                random.seed(0)
+                #np.random.seed(0)
+                #random.seed(0)
+
+
+                np.random.seed(0)  # Set the seed
+                x_original = np.random.rand(3, 32, 32)  # Some random image
+                x_noisy = gaussian_noise(x_original)  # Apply noise
+                
+                np.random.seed(0)  # Reset the seed
+                x_noisy_again = gaussian_noise(x_original)  # Apply noise again
+                
+                print(np.array_equal(x_noisy, x_noisy_again))
+
+                
                 
                 if task == 0 and i == 0:
                     print(batch_y, batch_x[0][0][0])
