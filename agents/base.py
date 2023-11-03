84a1a45d1d402e79609fccd935620064c6fd1ef1
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-03 05:58:20 +0330

    Update base.py

--- agents/base.py
+++ agents/base.py
@@ -127,9 +127,28 @@ class ContinualLearner(torch.nn.Module, metaclass=abc.ABCMeta):
                 batch_x = maybe_cuda(batch_x, self.cuda)
                 batch_y = maybe_cuda(batch_y, self.cuda)
                 
+                
+                np.random.seed(0)
+                random.seed(0)
+                torch.manual_seed(0)
+                torch.cuda.manual_seed(0)
+                torch.cuda.manual_seed_all(0)
+                torch.backends.cudnn.deterministic = True
+                torch.backends.cudnn.benchmark = False
+                
+                
                 #batch_x_ = batch_x[0]  # Taking the first image from the batch
-                #batch_x_pil = torchvision.transforms.functional.to_pil_image(torch.randn((3,32,32)).cpu())  # Convert to PIL image
-                #torch.tensor(gaussian_noise(batch_x_pil).astype(float) / 255.0, dtype = batch_x.dtype).to("cuda").permute(2,0,1).reshape(batch_x.shape)
+                batch_x_pil = torchvision.transforms.functional.to_pil_image(torch.randn((3,32,32)).cpu())  # Convert to PIL image
+                torch.tensor(gaussian_noise(batch_x_pil).astype(float) / 255.0, dtype = batch_x.dtype).to("cuda").permute(2,0,1).reshape(batch_x.shape)
+                
+                
+                np.random.seed(0)
+                random.seed(0)
+                torch.manual_seed(0)
+                torch.cuda.manual_seed(0)
+                torch.cuda.manual_seed_all(0)
+                torch.backends.cudnn.deterministic = True
+                torch.backends.cudnn.benchmark = False
                 
                 if task == 0 and i == 0:
                     print(batch_y, batch_x[0][0][0])
