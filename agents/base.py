0e9f0cfb05ce403692a172c64d5f2bc9575a9438
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-03 21:22:57 +0330

    Update base.py

--- agents/base.py
+++ agents/base.py
@@ -169,20 +169,22 @@ class ContinualLearner(torch.nn.Module, metaclass=abc.ABCMeta):
                     #print("Current NumPy Random Seed (Before):", np.random.get_state()[1][0])
                     #print("Current Torch Random Seed (Before):", torch.initial_seed())
                     #print("Current Torch CUDA Random Seed (Before):", torch.cuda.initial_seed())
-                    print("Current Torch Random Seed (Before):", torch.seed())
+                    
+                    current_seed = torch.seed()
+                    print("Current Torch Random Seed (Before):", current_seed)
                     before_seed = np.random.get_state()[1][0]
-                    before_cuda = torch.seed()
+                    #before_cuda = torch.seed()
                     to_pil = ToPILImage()
                     batch_x_ = batch_x[0]  # Taking the first image from the batch
                     batch_x_pil = to_pil(batch_x_.cpu())  # Convert to PIL image
                                         
                     batch_x111 = torch.tensor(self.gaussian_noise(batch_x_pil).astype(float) / 255.0, dtype = batch_x.dtype).to("cuda").permute(2,0,1).reshape(batch_x.shape)
                     np.random.seed(before_seed)
-                    torch.manual_seed(before_cuda)
+                    #torch.manual_seed(before_cuda)
                     #print("Current NumPy Random Seed (After):", np.random.get_state()[1][0])
                     #print("Current Torch Random Seed (After):", torch.initial_seed())
                     #print("Current Torch CUDA Random Seed (Before):", torch.cuda.initial_seed())
-                    print("Current Torch Random Seed (After):", torch.seed())
+                    print("Current Torch Random Seed (After):", current_seed)
                     
                     if self.params.trick['ncm_trick'] or self.params.agent in ['ICARL', 'SCR', 'SCP']:
                         feature = self.model.features(batch_x)  # (batch_size, feature_size)
