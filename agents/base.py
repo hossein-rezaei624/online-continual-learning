ded2535531d0ddce6db42fe3d1f6f183277adac8
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-03 21:06:55 +0330

    Update base.py

--- agents/base.py
+++ agents/base.py
@@ -167,7 +167,8 @@ class ContinualLearner(torch.nn.Module, metaclass=abc.ABCMeta):
                     batch_y = maybe_cuda(batch_y, self.cuda)
                     
                     #print("Current NumPy Random Seed (Before):", np.random.get_state()[1][0])
-                    print("Current Torch Random Seed (Before):", torch.initial_seed())
+                    #print("Current Torch Random Seed (Before):", torch.initial_seed())
+                    print("Current Torch CUDA Random Seed (Before):", torch.cuda.initial_seed())
                     before_seed = np.random.get_state()[1][0]
                     to_pil = ToPILImage()
                     batch_x_ = batch_x[0]  # Taking the first image from the batch
@@ -176,7 +177,8 @@ class ContinualLearner(torch.nn.Module, metaclass=abc.ABCMeta):
                     batch_x111 = torch.tensor(self.gaussian_noise(batch_x_pil).astype(float) / 255.0, dtype = batch_x.dtype).to("cuda").permute(2,0,1).reshape(batch_x.shape)
                     np.random.seed(before_seed)
                     #print("Current NumPy Random Seed (After):", np.random.get_state()[1][0])
-                    print("Current Torch Random Seed (After):", torch.initial_seed())
+                    #print("Current Torch Random Seed (After):", torch.initial_seed())
+                    print("Current Torch CUDA Random Seed (Before):", torch.cuda.initial_seed())
                     
                     
                     if self.params.trick['ncm_trick'] or self.params.agent in ['ICARL', 'SCR', 'SCP']:
