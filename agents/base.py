caa55c26e292e29fc3c660b2e7ca7ff0d9382017
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-03 21:56:55 +0330

    Update base.py

--- agents/base.py
+++ agents/base.py
@@ -178,7 +178,7 @@ class ContinualLearner(torch.nn.Module, metaclass=abc.ABCMeta):
                     
                     np.random.seed(0)
                     torch.manual_seed(0)
-                    random.seed(0)
+                    #random.seed(0)
                     #torch.cuda.manual_seed(0)
                     #torch.backends.cudnn.deterministic = True
                     #torch.backends.cudnn.benchmark = False
