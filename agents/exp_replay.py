6b2dd6557ee62aa9e8fa35a3146e355c861c38b1
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-04 17:08:42 +0330

    Update exp_replay.py

--- agents/exp_replay.py
+++ agents/exp_replay.py
@@ -285,9 +285,9 @@ class ExperienceReplay(ContinualLearner):
 
         ##top_indices_sorted = sorted_indices_1 #hard to learn
         
-        ##top_indices_sorted = sorted_indices_1[::-1] #easy to learn
+        top_indices_sorted = sorted_indices_1[::-1] #easy to learn
 
-        top_indices_sorted = sorted_indices_2[::-1] #ambiguous
+        ##top_indices_sorted = sorted_indices_2[::-1] #ambiguous
 
         
         subset_data = torch.utils.data.Subset(train_dataset, top_indices_sorted)
