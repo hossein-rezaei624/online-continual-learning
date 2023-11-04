aa8451e289c48d486cd00089d3e649bea5d53c95
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-04 05:58:44 +0330

    Update base.py

--- agents/base.py
+++ agents/base.py
@@ -218,7 +218,7 @@ class ContinualLearner(torch.nn.Module, metaclass=abc.ABCMeta):
                     grid = torchvision.utils.make_grid(images_display, nrow=len(images_display))  # Adjust nrow based on actual images
                     
                     # Save grid image with unique name for each batch
-                    torchvision.utils.save_image(grid, 'grid_image.png')
+                    torchvision.utils.save_image(grid, f'grid_image{i}.png')
 
 
 
