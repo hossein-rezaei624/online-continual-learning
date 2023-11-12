7bbb6ebe149991e35f14c4c8fa363dc57e93f590
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-12 08:46:35 +0330

    Update exp_replay.py

--- agents/exp_replay.py
+++ agents/exp_replay.py
@@ -120,7 +120,7 @@ class ExperienceReplay(ContinualLearner):
         
         
         ##plt.legend()
-        plt.savefig("tsneER")
+        plt.savefig("tsneGSS")
     
     
     def train_learner(self, x_train, y_train):
