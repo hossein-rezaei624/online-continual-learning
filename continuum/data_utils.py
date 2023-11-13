3e25cd2e48d4de0033810feba30991163390fda0
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-13 22:12:26 +0330

    Update data_utils.py

--- continuum/data_utils.py
+++ continuum/data_utils.py
@@ -51,7 +51,7 @@ class dataset_transform(data.Dataset):
         else:
             x = self.x[idx]
 
-        return x.float(), self.y[idx]
+        return x.float(), self.y[idx], idx
 
 
 def setup_test_loader(test_data, params):
