046b53ad44df5b2548a2837fe18bd7537b2ed64b
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-23 22:47:13 +0330

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -8,6 +8,7 @@ import torch.nn as nn
 from torch.nn.functional import relu, avg_pool2d
 from torch.autograd import Variable
 import torch
+from features.extractor import BaseModule
 def conv3x3(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
                      padding=1, bias=False)
