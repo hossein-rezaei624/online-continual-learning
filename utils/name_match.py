a9cd846a8a7662bf1a150cd36f22cc8b9b474589
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-23 21:30:53 +0330

    Update name_match.py

--- utils/name_match.py
+++ utils/name_match.py
@@ -5,12 +5,14 @@ from continuum.dataset_scripts.core50 import CORE50
 from continuum.dataset_scripts.mini_imagenet import Mini_ImageNet
 from continuum.dataset_scripts.openloris import OpenLORIS
 from agents.exp_replay import ExperienceReplay
+from agents.exp_replay_dvc import ExperienceReplay_DVC
 from agents.agem import AGEM
 from agents.ewc_pp import EWC_pp
 from agents.cndpm import Cndpm
 from agents.lwf import Lwf
 from agents.icarl import Icarl
 from agents.scr import SupContrastReplay
+from agents.pcr import ProxyContrastiveReplay
 from utils.buffer.random_retrieve import Random_retrieve
 from utils.buffer.reservoir_update import Reservoir_update
 from utils.buffer.mir_retrieve import MIR_retrieve
@@ -19,6 +21,7 @@ from utils.buffer.aser_retrieve import ASER_retrieve
 from utils.buffer.aser_update import ASER_update
 from utils.buffer.sc_retrieve import Match_retrieve
 from utils.buffer.mem_match import MemMatch_retrieve
+from utils.buffer.mgi_retrieve import MGI_retrieve
 
 data_objects = {
     'cifar100': CIFAR100,
@@ -30,6 +33,7 @@ data_objects = {
 
 agents = {
     'ER': ExperienceReplay,
+    'ER_DVC': ExperienceReplay_DVC,
     'EWC': EWC_pp,
     'AGEM': AGEM,
     'CNDPM': Cndpm,
@@ -37,10 +41,12 @@ agents = {
     'ICARL': Icarl,
     'GDUMB': Gdumb,
     'SCR': SupContrastReplay,
+    'PCR': ProxyContrastiveReplay,
 }
 
 retrieve_methods = {
     'MIR': MIR_retrieve,
+    'MGI':MGI_retrieve,
     'random': Random_retrieve,
     'ASER': ASER_retrieve,
     'match': Match_retrieve,
